0cb497ea0d6925e874357871a424d50a5b7be3d8
wubeZ <wubezeleke@gmail.com>
2022-09-15 21:52:54 +0300

    added logger and other dependencies

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,10 +1,11 @@
 import './common/env'
+import logger from './common/logger'
 import app from './app'
 import * as os from 'os'
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(
+    logger.info(
       `up and running in ${'development'
       } @: ${os.hostname()} on port ${PORT}`
     )
